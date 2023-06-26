6372f7a4dc13c0579bad33a2e682e310d61022c5
anntm2 <anntm2@vng.com.vn>
2023-06-26 19:46:57 +0700

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import '@mantine/core/styles.css';
+// import '@mantine/core/styles.css';
 import React from 'react';
-import { MantineProvider, ColorSchemeScript } from '@mantine/core';
+import Providers from './provider';
 
 export const metadata = {
   title: 'Mantine Next.js template',
@@ -11,11 +11,10 @@ export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <head>
-        <ColorSchemeScript />
         <link rel="shortcut icon" href="/favicon.svg" />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { Container, Overlay, BackgroundImage, Group, Title, Box, em } from '@mantine/core';
+import { Group, BackgroundImage, Title, Container, SimpleGrid, Box, Flex, Stack, Grid, Image } from '@mantine/core'
 import { createStyles, rem } from '@mantine/styles';
-import Image from 'next/image';
 
 const useStyles = createStyles(() => ({
   wrapper: {
@@ -41,10 +40,34 @@ const Main = () => {
   const { classes } = useStyles();
   return (
     <BackgroundImage src="/background.png" className={classes.wrapper}>
+      {/* Header */}
       <Container className={classes.header} fluid>
         <Container size="lg" px="xs">
           <LogoName />
         </Container>
+      </Container>
+
+      <Container size='lg' px='xs' mt={20}>
+        <Grid columns={5} mih='60vh'>
+          <Grid.Col span={3}>
+            <Flex direction={'column'} h={'100%'} justify={'center'} gap={'md'}>
+              <Title order={1} className={classes.brandName}>
+                Trải nghiệm Tivi, Bóng đá, IPTV miễn phí - Giải trí trong lòng bàn tay!
+              </Title>
+              <Title order={3}>
+                Xem gần 200 kênh Truyền hình trong nước và quốc tế
+                <br/>
+                <br/>
+                Trực tiếp bóng đá Ngoại Hạng Anh, UEFA Champions League, LaLiga, Ligue 1, Bundesliga, V League,...
+              </Title>
+            </Flex>
+          </Grid.Col>
+          <Grid.Col span={2}>
+            <Flex direction={'column'} h={'100%'} justify={'center'}>
+              <Image src='/introduce.png' alt='image' mx='auto' />
+            </Flex>
+          </Grid.Col>
+        </Grid>
       </Container>
     </BackgroundImage>
   );
