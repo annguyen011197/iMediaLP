4788405d1fb141e56acf0ccee6bb9af5ec7175e7
An Nguyen <tranminhannguyen@gmail.com>
2023-08-04 02:45:31 +0700

    Config assets

--- next.config.js
+++ next.config.js
@@ -2,10 +2,33 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: false,
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-});
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+
+  module.exports = withBundleAnalyzer({
+    reactStrictMode: false,
+    output: 'export',
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+  });
+} else {
+  module.exports = withBundleAnalyzer({
+    reactStrictMode: false,
+    eslint: {
+      ignoreDuringBuilds: true,
+    }
+  });
+}
+
+
